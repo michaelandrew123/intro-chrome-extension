b5c46c42a8fffe2b4236c6f8f531f17bdab7451b
Michael Andrew Suarez <michaelandrewsuarez0@gmail.com>
2021-01-04 16:17:35 +0800

    initial commit

--- background.js
+++ background.js
@@ -9,6 +9,7 @@ chrome.tabs.onActivated.addListener(tab => {
         active_tab_id = tab.tabId;
         //console.log(current_tab_info.url);
         if(/^https:\/\/www\.google/.test(current_tab_info.url)){ 
+           // https://www.google.com/
             chrome.tabs.insertCSS(null, {file: './mystyle.css'})
             chrome.tabs.executeScript(null, {file: './foreground.js'}, ()=> console.log("I injected"))
         } 
